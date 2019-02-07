6b9efa7db7ec720e91a8c94dd1a5f4a40badd6b5
Alex Bruce <brucealmightyalex@yahoo.com>
2019-02-06 17:11:43 -0700

    Add professional content to About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,17 +5,17 @@ class About extends Component {
   render() {
     return (
       <div className='About'>
-        <article>
+        <article className='about-professional'>
           <h1>Hi, I'm Alex</h1>
           <p>
             I'm a front-end software developer with a passion for problem-solving, collaboration, music, dogs, and fast cars. I've always loved solving puzzles and figuring out how things work, which has occasionally led to taking my car apart just to see what makes it tick. I've, more recently, discovered a similar love for JavaScript. I enjoy a good challenge, and live for the 'builder's high' feeling after completing something. Now, creating clean, cohesive, and powerful UIs is what occupies most of my time.
           </p>
-          <h3>education</h3>
+          <h3>Education</h3>
           <p>
-            On January 10, 2019, I graduated from the Turing School of Software and Design in Denver, Colorado. I'm a graduate of their Front-End Engineering program, where I learned the fundamentals of development, including Languages, frameworks, and libraries, as well as version control, unit testing,continuous integration, and deployment.
+            On January 10, 2019, I graduated from the Turing School of Software and Design in Denver, Colorado. I'm a graduate of their Front-End Engineering program, where I learned the fundamentals of development, including Languages, frameworks, and libraries, as well as version control, unit testing, continuous integration, and deployment.
           </p>
 
-          <h3>tech stack</h3>
+          <h3>Tech Stack</h3>
           <ul>
             <li>HTML5</li>
             <li>Javascript (ES5, ES6, ES7 +)</li>
@@ -28,10 +28,11 @@ class About extends Component {
             <li>Node</li>
             <li>Express</li>
             <li>Knex</li>
+            <li>SQL</li>
             <li>Object Oriented Programming</li>
           </ul>
 
-          <h3>other technologies</h3>
+          <h3>Other Technologies</h3>
           <ul>
             <li>Version Control (Git)</li>
             <li>Terminal</li>
@@ -39,6 +40,12 @@ class About extends Component {
             <li>Adobe Photoshop</li>
           </ul>
 
+        </article>
+        <article className='about-personal'>
+          <h2>who I am</h2>
+          <p>
+            asdfgh
+          </p>
         </article>
       </div>
     )
