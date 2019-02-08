bfea057005ed132a5526982451492435fac958fe
Alex Bruce <brucealmightyalex@yahoo.com>
2019-02-07 18:27:18 -0700

    Add more functionality to About and Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,16 @@ class Header extends Component {
   render() {
     return (
       <div className='Header'>
-        <div className='header-left'>
-          <img className='header-logo' src={logo} alt=''/>
-          <button className='header-name'>alex bruce</button>
-        </div>
-        <div className='header-right'>
-          <button className='header-about header-button'>about</button>
-          <button className='header-portfolio header-button'>portfolio</button>
-          <button className='header-contact header-button'>contact</button>
+        <div className='header-main'>
+          <div className='header-left'>
+            <img className='header-logo' src={logo} alt=''/>
+            <button className='header-name'>alex bruce</button>
+          </div>
+          <div className='header-right'>
+            <button className='header-about header-button'>about</button>
+            <button className='header-portfolio header-button'>portfolio</button>
+            <button className='header-contact header-button'>contact</button>
+          </div>
         </div>
       </div>
     )
