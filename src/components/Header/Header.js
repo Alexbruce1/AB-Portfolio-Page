df0eff633fbe1a156c1d42a82a2264fb798384d7
Alex Bruce <brucealmightyalex@yahoo.com>
2019-02-08 11:48:44 -0700

    Add React Router and establish basic page structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Header.scss';
 import logo from '../../Images/logo.png';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
   render() {
@@ -8,13 +9,23 @@ class Header extends Component {
       <div className='Header'>
         <div className='header-main'>
           <div className='header-left'>
-            <img className='header-logo' src={logo} alt=''/>
-            <button className='header-name'>alex bruce</button>
+            <NavLink title='Alex Bruce' to='/' className='navlink'>
+              <img className='header-logo' src={logo} alt=''/>
+            </NavLink>
+            <NavLink title='Alex Bruce' to='/' className='navlink'>
+              <button className='header-name'>alex bruce</button>
+            </NavLink>
           </div>
           <div className='header-right'>
-            <button className='header-about header-button'>about</button>
-            <button className='header-portfolio header-button'>portfolio</button>
-            <button className='header-contact header-button'>contact</button>
+            <NavLink title='About' to='/' className='navlink'>
+              <button className='header-about header-button'>about</button>
+            </NavLink>
+            <NavLink title='Portfolio' to='/portfolio' className='navlink'>
+              <button className='header-portfolio header-button'>portfolio</button>
+            </NavLink>
+            <NavLink title='Contact' to='/contact' className='navlink'>
+              <button className='header-contact header-button'>contact</button>
+            </NavLink>
           </div>
         </div>
       </div>
